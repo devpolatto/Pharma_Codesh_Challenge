50e30c130253e0fceb6b26057e8926e4f56da408
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-10-09 17:03:06 -0300

    add style global

---
+++ src/styles/Global.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+  :root{
+        --background: #FEFCFF;
+        --background-header: #FEFEFF;
+        --Background-sidebar: #142A52;
+        --color-icons: #64B899;
+        --color-text: #404751;
+
+    }
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body{
+        background: var(--background);
+    }
+    
+    /* body, input, textarea, button{
+        font: 400 1rem "Inter", sans-serif;
+    } */
+    button{
+        cursor: pointer;
+    }
+    a{
+        text-decoration: none;
+    }
+`;
