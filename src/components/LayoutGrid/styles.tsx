c868d4c54a5d3be598815f95d71417ab78080103
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-10-09 17:20:45 -0300

    fixed name grid-area

--- src/components/LayoutGrid/styles.tsx
+++ src/components/LayoutGrid/styles.tsx
@@ -4,12 +4,12 @@ export const GridContainer = styled.div`
     display: grid;
     grid-template-columns: 320px auto;
     grid-template-rows: 75px auto;
-    // SN -> SideBar
+    // SB -> SideBar
     // HR -> Header
     // MN -> Main
     grid-template-areas: 
-        'SN HR'
-        'SN MN';
+        'SB HR'
+        'SB MN';
     height: 100vh;
     width: 100vw;
  
