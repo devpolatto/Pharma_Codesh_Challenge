5eb04368d015ecca7202a34c7b17d50f9a1fe289
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-10-09 22:15:07 -0300

    add Logo img at Header

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import Logo from '../../assests/Logo.svg';
+
 const SideBar: React.FC = () => {
   return(
       <Container>
-          
+          <img src={Logo} alt="" />
       </Container>
   );
 }
--- src/components/SideBar/styles.tsx
+++ src/components/SideBar/styles.tsx
@@ -3,5 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   grid-area: SB;
 
-  background-color: var(--Background-sidebar);
+  background-color: #142A52;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 16px 0;
 `;
